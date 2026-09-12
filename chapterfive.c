910db12e8ef30369283870f029372d1a382f1a30
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-09-13 00:18:09 +0530

    recursion

--- chapterfive.c
+++ chapterfive.c
@@ -160,7 +160,7 @@
 //     sum
 // }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int factorial(int a);
 
@@ -177,11 +177,39 @@ int factorial(int a){
     for(int i = 1;i<=a;i++){
        
 
-   int  c=  i*c; 
-    printf("factorial is - %d", c);
+     c=  c*i; 
+   printf(" %d\n", c);
 
     }
     printf("factorial is - %d", c);
+    
+    return c;
+}    */
+
+#include <stdio.h>
+
+int fact(int a);
+
+int main(){
+    int a ;
+    printf("enter a number - ");
+    scanf("%d",&a);
+printf("factorial is - %d",fact(a));
+return 0 ;
 }
+
+int fact(int a){
+    
+    if(a==1){ 
+
+        return 1 ; }
+
+ int factnm = fact(a-1);
+ int factn = factnm*a ;
+    
+ return factn ;
+    
+}
+
 
 
