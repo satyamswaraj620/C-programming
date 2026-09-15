352fa4aef5d4716886cf5eaa8e3f8ddfe4a94e10
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-09-16 02:27:07 +0530

    Array

---
+++ chapterseven.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+//int main (){
+
+   /* int age[2];
+    printf("Satyam enter your age :");
+    scanf("%d",&age[0]);
+
+    printf("himanshu enter your age : ");
+    scanf("%d",&age[1]);
+
+    printf("satyam age = %d  , himanshu age = %d ", age[0],age[1]);
+    return 0 ;*/
+
+    void gst(float a , float b , float c );
+
+    int main(){
+
+ float price[3];
+ printf("enter book price :");
+    scanf("%f",&price[0]);
+
+    printf("enter chocolate price : ");
+    scanf("%f",&price[1]);
+
+    printf("enter clothes price : ");
+    scanf("%f",&price[2]);
+
+
+    gst(price[0],price[1],price[2]);
+return 0;    
+}
+    void gst(float a, float b, float c ){
+
+        float gst1 = a + (0.18*a);
+          float gst2 = b + (0.18*b);
+            float gst3 = c + (0.18*c);
+
+            printf("price of book with gst is = %f \n", gst1);
+              printf("price of chocolate with gst is = %f \n", gst2);
+                printf("price of clothes with gst is = %f \n", gst3);
+                
+
+    }
+
+
+
+   
