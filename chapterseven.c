9209ca6a02cfe446c3c3f70a6f5309d94ba93f7f
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-09-17 11:08:32 +0530

    array

--- chapterseven.c
+++ chapterseven.c
@@ -1,48 +1,64 @@
 #include <stdio.h>
 
-//int main (){
+// int main (){
 
-   /* int age[2];
-    printf("Satyam enter your age :");
-    scanf("%d",&age[0]);
+/* int age[2];
+ printf("Satyam enter your age :");
+ scanf("%d",&age[0]);
 
-    printf("himanshu enter your age : ");
-    scanf("%d",&age[1]);
+ printf("himanshu enter your age : ");
+ scanf("%d",&age[1]);
 
-    printf("satyam age = %d  , himanshu age = %d ", age[0],age[1]);
-    return 0 ;*/
+ printf("satyam age = %d  , himanshu age = %d ", age[0],age[1]);
+ return 0 ;*/
 
-    void gst(float a , float b , float c );
+//     void gst(float a , float b , float c );
 
-    int main(){
+//     int main(){
 
- float price[3];
- printf("enter book price :");
-    scanf("%f",&price[0]);
+//  float price[3];
+//  printf("enter book price :");
+//     scanf("%f",&price[0]);
 
-    printf("enter chocolate price : ");
-    scanf("%f",&price[1]);
+//     printf("enter chocolate price : ");
+//     scanf("%f",&price[1]);
 
-    printf("enter clothes price : ");
-    scanf("%f",&price[2]);
-                             
+//     printf("enter clothes price : ");
+//     scanf("%f",&price[2]);
 
-    gst(price[0],price[1],price[2]);
-return 0;    
+//     gst(price[0],price[1],price[2]);
+// return 0;
+// }
+//     void gst(float a, float b, float c ){
+
+//         float gst1 = a + (0.18*a);
+//           float gst2 = b + (0.18*b);
+//             float gst3 = c + (0.18*c);
+
+//             printf("price of book with gst is = %f \n", gst1);
+//               printf("price of chocolate with gst is = %f \n", gst2);
+//                 printf("price of clothes with gst is = %f \n", gst3);
+
+//     }
+
+#include <stdio.h>
+
+// int main()
+// {
+//   int adhaar[5];
+//      int *ptr = &adhaar[0] ;
+//      for(int i = 0;i<5;i++){
+//       printf("index %d ",i);
+//       scanf("%d",ptr+i);        //ptr+i = adhaar[i]
+//      }
+
+//      for(int i = 0 ; i<5;i++){
+//      printf("adhaar no - %d\n",*(ptr+i));  //*(ptr+1) = adhaar[i]
+
+//      }
+//      return 0 ;
+// }
+
+int main(){
+  
 }
-    void gst(float a, float b, float c ){
-
-        float gst1 = a + (0.18*a);
-          float gst2 = b + (0.18*b);
-            float gst3 = c + (0.18*c);
-
-            printf("price of book with gst is = %f \n", gst1);
-              printf("price of chocolate with gst is = %f \n", gst2);
-                printf("price of clothes with gst is = %f \n", gst3);
-                
-
-    }
-
-
-
-   
